70dfefba2b2a3b3af465e3b780ab06b4c13a8314
samstha07 <samshrestha85@gmail.com>
2022-01-26 00:59:14 +0545

    removing console.log from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,10 @@ if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = function () {};
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
