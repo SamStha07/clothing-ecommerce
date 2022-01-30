d2eb85fa98ca81d8460ea5449ee3737080e1b8d2
samstha07 <samshrestha85@gmail.com>
2022-01-30 22:43:16 +0545

    added Profiler for performance checking

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import Directory from 'src/components/directory/directoy.component';
 import { HomepageContainer } from './homepage.styles';
 
 const HomePage = () => {
   return (
     <HomepageContainer>
-      <Directory />
+      <Profiler
+        id='directoy'
+        onRender={(id, phase, actucalDuration) => {
+          console.log({ id, phase, actucalDuration });
+        }}
+      >
+        <Directory />
+      </Profiler>
     </HomepageContainer>
   );
 };
